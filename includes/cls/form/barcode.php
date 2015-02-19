dbd96614a52a2d1974178887e6f88fa742e46300
Javad <J.Evazzadeh@live.com>
2015-02-19 17:58:04 +0330

    some fix for usiblity

--- includes/cls/form/barcode.php
+++ includes/cls/form/barcode.php
@@ -7,11 +7,10 @@ class barcode extends \lib\form
 	{
 		$this->barcode = $this->make("number")->name("barcode")->type("number")
 						->min(10000)->max(99999)->pattern('.{5,}')->class('span8')
-						->tabindex(1)->autofocus()
-						->pos('hint-bottom hint-always')->desc(T_('Please Scan or enter barcode'));
+						->tabindex(1)->autofocus()->title(T_('Please Scan or enter barcode'));
 
 
-		$this->submit	= $this->make('submit')->value(T_('Check'))->title(T_('Check'))
+		$this->submit	= $this->make('submit')->value(T_('Check'))->title(T_('Click to check'))
 						->tabindex(2)
 						->class('span4');
 	}
