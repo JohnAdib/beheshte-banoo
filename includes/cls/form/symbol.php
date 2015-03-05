e610032ee3b343a84bc4affbeba84cfe2cef01af
Javad <J.Evazzadeh@live.com>
2015-03-05 23:52:30 +0330

    little fix on style

--- includes/cls/form/symbol.php
+++ includes/cls/form/symbol.php
@@ -16,7 +16,7 @@ class symbol extends \lib\form
 							->required()->maxlength(17)->pattern(".{9,}");
 
 		$this->mobile 	= $this->make("mobile")->name('mobile')->type("tel")->label(T_("Mobile"))->pl(T_("Mobile"))
-							// ->pos('hint--rounded hint--top')->desc(T_("we send a verification code to this number"))
+							->pos('hint--rounded hint--top')->desc(T_("we send a verification code to this number"))
 							->required()->maxlength(17)->pattern(".{10,}")->autocomplete('off');
 
 		$this->pass =
