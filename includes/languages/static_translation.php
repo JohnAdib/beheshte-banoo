9b7197d0d40be81af2cc59a8bfeb3294d63b3a60
Javad <J.Evazzadeh@live.com>
2015-03-06 16:38:38 +0330

    fix translation

--- includes/languages/static_translation.php
+++ includes/languages/static_translation.php
@@ -4,6 +4,7 @@ function transtext()
 	// add static text for detect poedit translate
 	// Saloos Default data
 	echo T_('Saloos');
+	echo T_('saloos');
 	echo T_('Another Project with Saloos');
 	echo T_('Saloos is an artichokes for PHP programming!!');
 	echo T_('Saloos is powerfull.');
@@ -24,70 +25,39 @@ function transtext()
 	echo T_('you account is verified successfully');
 	echo T_('thanks for using our service');
 	echo T_('made in iran');
-
-	// countries
-	echo T_('Iran (Islamic Republic of)');
-	echo T_('India');
-	echo T_('Iraq');
-	echo T_('Lebanon');
-	echo T_('Afghanistan');
-	echo T_('Azerbaijan');
-	echo T_('Bahrain');
-	echo T_('United Arab Emirates');
-	echo T_('Uzbekistan');
-	echo T_('Turkey');
-	echo T_('Saudi Arabia');
-	echo T_('Singapore');
-	echo T_('Pakistan');
-	echo T_('Qatar');
-	echo T_('United States');
-	
-	
-
-	
-	
-	//----------------------------------------------------content_cp/home/display.html
-	echo T_("Logout");                                                                // Line 43
-	echo T_("Home");                                                                  // Line 40
-
-	//-----------------------------------------------------content_cp/main/layout.html
-	echo T_("Options");                                                               // Line 24 Seperate
-	echo T_("Go to");                                                                 // Line 68
-	echo T_("Does not exist");                                                        // Line 32 Seperate
-	echo T_("Add New Record");                                                        // Line 39 Seperate
-	echo T_("Actions");                                                               // Line 52
-	echo T_("Edit");                                                                  // Line 71
-	echo T_("Delete");                                                                // Line 74
-	
-
-
-	// For Datatable	
-	echo T_('Next page');
-
-	// Your new static text that does not exist on this project! Add them manually
+	echo T_('Iran');
+	echo T_('iran');
+	echo T_('submit');
+	echo T_('save');
+	echo T_('add new');
+	echo T_('signin');
+	echo T_('sign in');
+	echo T_('signup');
+	echo T_('sign up');
+	echo T_('register');
+	echo T_('create an account');
 	echo T_('home');
+	echo T_('Home');
+	echo T_('homepage');
 	echo T_('Homepage');
-	echo T_('Signup');
-	echo T_('Add new');
-	echo T_('Add New');
-	echo T_('Admin');
-	echo T_('Visitor');
+	echo T_('admin');
+	echo T_('error in field');
+	/**
+	**
+	**
+	**
+	** Your new static text that does not exist on this project! Add them manually in below lines
+	**/
 	echo T_('Boother');
-	echo T_('Registerer');
-	
-	echo T_('Register');
+	echo T_('Registerer');	
 	echo T_('Camera');
 	echo T_('Game');
 	echo T_('Point');
-	echo T_('Kid');
 	echo T_('Report');
-	echo T_('birthdate');
-	echo T_('gender');
-	echo T_('error in field');
-	echo T_('submit');
-	echo T_('Submit');
-	echo T_('save');
-	echo T_('Save');
-	
+	echo T_('Kid');
+	echo T_('change');
+
+
+
 }
 ?>
