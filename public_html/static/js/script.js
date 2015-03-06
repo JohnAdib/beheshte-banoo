f9af0040c3ee2209379fce2beb2247fd9fe50263
Mahdi Dibaiee <mdibaiee@aol.com>
2015-03-06 17:24:29 +0330

    bigi ke oomad, ye galle fix

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -1,14 +1,3 @@
-// Find the right method, call on correct element
-function launchFullscreen(element) {
-  if(element.requestFullscreen) {element.requestFullscreen();
-  } else if(element.mozRequestFullScreen) {element.mozRequestFullScreen();
-  } else if(element.webkitRequestFullscreen) {element.webkitRequestFullscreen();
-  } else if(element.msRequestFullscreen) {element.msRequestFullscreen();
-  }
-}
-
-
-
 // ***************************************************** Kids
 function bindKids() {
   $('#form-kids').on('ajaxify:success', function() {
