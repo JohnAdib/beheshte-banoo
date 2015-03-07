88a6876b8ee18d6633c7aa6f6e91d68bdd5f742c
Javad <J.Evazzadeh@live.com>
2015-03-07 11:53:57 +0330

    test fullscreen page on android

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -278,3 +278,7 @@ $("#mobile").bind('keypress input', function(e)
 });
 // *********************************************************** end handle keypress
 
+
+if(navigator.userAgent.match(/Android/i)){
+    window.scrollTo(0,1);
+ }
