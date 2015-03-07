7191428f282b140b22119931cd57138a2e5321f9
Javad <J.Evazzadeh@live.com>
2015-03-07 11:58:40 +0330

    test 2 for fullscreen on android chrome (reverted from commit 6ddeebfa401b72a9f7c192263de20b21e3921309)

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -278,11 +278,3 @@ $("#mobile").bind('keypress input', function(e)
 });
 // *********************************************************** end handle keypress
 
-
-function hideAddressBar(){
-  if(document.documentElement.scrollHeight<window.outerHeight/window.devicePixelRatio)
-    document.documentElement.style.height=(window.outerHeight/window.devicePixelRatio)+'px';
-  setTimeout(window.scrollTo(1,1),0);
-}
-window.addEventListener("load",function(){hideAddressBar();});
-window.addEventListener("orientationchange",function(){hideAddressBar();});
